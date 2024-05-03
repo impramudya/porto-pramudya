28a21704f399c2b7692bb814ba886e7d1512bb85
pramudya <itsme.pramudya@gmail.com>
2024-05-03 11:04:25 +0700

    push

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '32c6c0d2fd71b0cab5468ad1963e1aa1c3cf6eeb',
+        'reference' => '2b534a216bde0093b71420f56d03fde77f42992a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '32c6c0d2fd71b0cab5468ad1963e1aa1c3cf6eeb',
+            'reference' => '2b534a216bde0093b71420f56d03fde77f42992a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
